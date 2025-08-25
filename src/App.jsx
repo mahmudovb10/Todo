797f5f08b796cd1568946f0a01270a425b45d1b9
Bahodirxon <bohodirxon.mahmudoov@gmail.com>
2025-08-25 10:39:48 +0500

    Updated App.jsx and Index.css

--- src/App.jsx
+++ src/App.jsx
@@ -60,50 +60,31 @@ function App() {
   return (
     <div className="mt-20 customContainer">
       <div className="mainDiv">
-        <h1 className="flex justify-center">Todo</h1>
+        <h1 className="flex justify-center title">Todo</h1>
         <form className="flex justify-center" onSubmit={handleSubmit}>
-          <div className="flex flex-col items-center justify-center relative left-[6rem] mainImgLoc">
-            <label>Title: </label>
+          <div className="flex flex-col items-center justify-center relative left-[6rem] mainInp">
             <input
               value={_title}
               onChange={(e) => setTitle(e.target.value)}
               name="title"
               type="text"
-              className="input"
+              className="input mainInput"
               placeholder="Create a new todo…"
             />
           </div>
           <div className="relative top-[5rem] right-[3rem]">
-            <label>Completed: </label>
+            {/* <label>Completed: </label>
             <input
               defaultChecked={_completed}
               type="checkbox"
               name="completed"
               className="checkbox checkbox-primary"
-            />
+            /> */}
           </div>
-          <button className="btn btn-outline btn-primary relative top-[8rem] right-[9rem]">
+          {/* <button className="btn btn-outline btn-primary relative top-[8rem] right-[9rem]">
             Add Todo
-          </button>
+          </button> */}
         </form>
-      </div>
-      <div className="filterBtns">
-        <button className="filterAll" onClick={() => dispatch(filter("all"))}>
-          All
-        </button>
-        <button
-          className="filterActive"
-          onClick={() => dispatch(filter("active"))}
-        >
-          Active
-        </button>
-        <button
-          className="filterCompleted"
-          onClick={() => dispatch(filter("completed"))}
-        >
-          Completed
-        </button>
-        <div className="todoCount">Items left: {todos.length}</div>
       </div>
       <ul className="todoList">
         {todos &&
@@ -122,7 +103,6 @@ function App() {
                   )
                 }
               />
-
               <h4
                 className="todoItem"
                 onDoubleClick={() => handleEdit(todo)}
@@ -130,7 +110,6 @@ function App() {
               >
                 {todo.title}
               </h4>
-
               <button
                 className="deleteBtn"
                 onClick={() => dispatch(removeTodo(todo.id))}
@@ -139,7 +118,26 @@ function App() {
               </button>
             </li>
           ))}
+        <hr className="customHr" />
       </ul>
+      <div className="filterBtns">
+        <button className="filterAll" onClick={() => dispatch(filter("all"))}>
+          All
+        </button>
+        <button
+          className="filterActive"
+          onClick={() => dispatch(filter("active"))}
+        >
+          Active
+        </button>
+        <button
+          className="filterCompleted"
+          onClick={() => dispatch(filter("completed"))}
+        >
+          Completed
+        </button>
+        <div className="todoCount">Items left: {todos.length}</div>
+      </div>
     </div>
   );
 }
